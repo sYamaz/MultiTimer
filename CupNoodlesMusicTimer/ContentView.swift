31c248174628fac0a6bd01795745b246b5402480
shun yamazaki <33094775+SyunYamazaki@users.noreply.github.com>
2021-02-16 14:34:58 +0900

    Initial Commit

---
+++ CupNoodlesMusicTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupNoodlesMusicTimer
+//
+//  Created by 山﨑駿 on 2021/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupNoodlesMusicTimer/CupNoodlesMusicTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupNoodlesMusicTimerApp.swift
+//  CupNoodlesMusicTimer
+//
+//  Created by 山﨑駿 on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct CupNoodlesMusicTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
